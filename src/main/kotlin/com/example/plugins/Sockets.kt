76fed20fa63d2ed042c52774b34c0d6d7cd31f31
goosLy1 <gray.goosly@gmail.com>
2024-12-08 23:39:02 +0300

    Add receiving  client ID from outside

--- src/main/kotlin/com/example/plugins/Sockets.kt
+++ src/main/kotlin/com/example/plugins/Sockets.kt
@@ -5,7 +5,6 @@ import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import kotlinx.coroutines.channels.consumeEach
-import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.time.Duration.Companion.seconds
 
@@ -21,7 +20,14 @@ fun Application.configureSockets() {
 
     routing {
         webSocket("/ws") { // websocketSession
-            val clientId = UUID.randomUUID().toString()
+//            val clientId = UUID.randomUUID().toString()
+
+            val clientId = call.request.queryParameters["clientId"]
+            if (clientId.isNullOrEmpty()) {
+                close(CloseReason(CloseReason.Codes.CANNOT_ACCEPT, "Missing clientId"))
+                return@webSocket
+            }
+
             connections[clientId] = this
             println("Client $clientId connected")
 
